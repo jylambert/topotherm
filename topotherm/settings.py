caf23fdb07373ded7d2cdd951ac39edc46046b83
Amedeo Ceruti <amedeo.ceruti@tum.de>
2024-05-14 17:13:41 +0200

    fix dataclass bug with default_factory lambda func

--- topotherm/settings.py
+++ topotherm/settings.py
@@ -1,7 +1,8 @@
 """This file contains all the settings for the optimization problem and should be modified
 and adapted to each case."""
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Tuple
 
 @dataclass
 class Water:
@@ -28,20 +29,21 @@ class Piping:
     """Piping properties for thermal losses and investment cost linearization regression."""
     # list of all available diameters
     # list of floats of all inner diameters of the available discrete pipe sizes
-    diameter: tuple = (
+    diameter: Tuple[float, ...] = field(default_factory=lambda: (
         0.0216, 0.0285, 0.0372, 0.0431, 0.0545, 0.0703, 0.0825, 0.1071, 0.1325, 0.1603, 0.2101,
         0.263, 0.3127, 0.3444, 0.3938
-        )
-    # pipe outer diameter in m
-    outer_diameter: tuple = (
-        0.09, 0.09, 0.11, 0.11, 0.125, 0.14, 0.16, 0.2, 0.225, 0.25, 0.315, 0.4, 0.45, 0.5, 0.56)
-    # pipe costs
-    cost: tuple = (
-        390, 400, 430, 464, 498, 537, 602, 670, 754, 886, 1171, 1184, 1197, 1401, 1755)
+    ))
+    outer_diameter: Tuple[float, ...] = field(default_factory=lambda: (
+        0.09, 0.09, 0.11, 0.11, 0.125, 0.14, 0.16, 0.2, 0.225, 0.25, 0.315, 0.4, 0.45, 0.5, 0.56
+    ))
+    cost: Tuple[float, ...] = field(default_factory=lambda: (
+        390, 400, 430, 464, 498, 537, 602, 670, 754, 886, 1171, 1184, 1197, 1401, 1755
+    ))
+    number_diameter: int = 15  # number of discrete diameters
     max_pr_loss: int = 250  # assumed pressure loss in Pa per meter
     roughness: float = 0.05e-3  # pipe roughtness factor
     thermal_conductivity: float = 0.024  # pipe thermal conductivity in W/mK
-    number_diameter: int = len(diameter)  # number of discrete diameters
+    
 
 
 @dataclass
@@ -64,13 +66,14 @@ class Economics:
 @dataclass
 class Optimization:
     """Settings with subclasses for the optimization problem."""
-    opt_settings: OptSettings = OptSettings()
-    economics: Economics = Economics()
-    temperatures: Temperatures = Temperatures()
+    opt_settings: OptSettings = field(default_factory=OptSettings)
+    economics: Economics = field(default_factory=Economics)
+    temperatures: Temperatures = field(default_factory=Temperatures)
 
+@dataclass
 class Regression:
     """Settings for the linearization of the piping."""
-    ground: Ground = Ground()
-    water: Water = Water()
-    temperatures: Temperatures = Temperatures()
-    piping: Piping = Piping()
+    ground: Ground = field(default_factory=Ground)
+    water: Water = field(default_factory=Water)
+    temperatures: Temperatures = field(default_factory=Temperatures)
+    piping: Piping = field(default_factory=Piping)
