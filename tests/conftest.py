2289deba26bf80e122bfffdb696a177c01e8baca
Amedeo Ceruti <amedeo.ceruti@tum.de>
2024-07-19 15:45:07 +0200

    rename tests, update to cbc, add callable solver name in tests

---
+++ tests/conftest.py
@@ -0,0 +1,3 @@
+def pytest_addoption(parser):
+    parser.addoption("--solver", action="store", default="scip",
+                     help="Define the solver to use for the optimization. Default is cbc.")
