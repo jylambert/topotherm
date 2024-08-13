1eed3dc95e4b14d575a8555fe34359416f3940ae
Amedeo Ceruti <amedeo.ceruti@tum.de>
2024-08-13 15:41:03 +0200

    improved diam, velocity equation

--- topotherm/postprocessing.py
+++ topotherm/postprocessing.py
@@ -7,44 +7,45 @@ This module includes the following functions:
 
 import numpy as np
 import pyomo.environ as pyo
-from scipy.optimize import fsolve
+from scipy.optimize import fsolve, root
 import networkx as nx
 
 from topotherm.settings import Settings
 
 
 def sts(model: pyo.ConcreteModel,
-                matrices: dict,
-                settings: Settings):
+        matrices: dict,
+        settings: Settings):
     """Create variables for the thermo-hydraulic coupled optimization.
 
     Args:
         model (pyo.ConcreteModel): pyomo model
         matrices (dict): dict containing the matrices
         sets (dict): dict containing the sets
-        mode (str): sts or mts
-
     
     Returns:
-        _type_: dict containing the variables
+        dict: containing the variables and postprocessed data
     """
+    # Get the values from the model
     p_ij = np.array(pyo.value(model.P['ij', 'in', :, :]))
     p_ji = np.array(pyo.value(model.P['ji', 'in', :, :]))
-    lambda_ij = np.array(pyo.value(model.lambda_['ij', :]))
-    lambda_ji = np.array(pyo.value(model.lambda_['ji', :]))
+    # flow direction, binary
+    lambda_ij = np.around(
+        np.array(pyo.value(model.lambda_['ij', :])),
+        0)
+    lambda_ji = np.around(
+        np.array(pyo.value(model.lambda_['ji', :])),
+        0)
 
-    lambda_dir_1 = np.around(lambda_ij, 0)
-    lambda_dir_2 = np.around(lambda_ji, 0)
-
-    # Restart, Adaption of Incidence Matrix for the thermo-hydraulic coupled optimization
+    # Adaption of Incidence Matrix for thermo-hydraulic coupled optimization
     for q, _ in enumerate(lambda_ij):
-        if lambda_dir_1[q] == 0 and lambda_dir_2[q] == 0:
+        if lambda_ij[q] == 0 and lambda_ji[q] == 0:
             matrices['a_i'][:, q] = 0
             matrices['l_i'][q] = 0
-        elif lambda_dir_2[q] == 1:
+        elif lambda_ji[q] == 1:
             matrices['a_i'][:, q] = matrices['a_i'][:, q] * (-1)
 
-    p_lin = p_ij + p_ji
+    p_lin = p_ij + p_ji  # Power of the pipes
 
     # drop entries with 0 in the incidence matrix to reduce size
     valid_columns = matrices['a_i'].any(axis=0)
@@ -62,7 +63,11 @@ def sts(model: pyo.ConcreteModel,
     v_lin2 = np.zeros(a_i_shape_opt[1])
     supply_temp_opt = np.ones(a_i_shape_opt[1]) * settings.temperatures.supply
     return_temp_opt = np.ones(a_i_shape_opt[1]) * settings.temperatures.return_
+
     def equations(v):
+        """Equations for the calculation of the diameter and velocity of
+        the pipes depending on the mass flow and the power of the pipes.
+        """
         vel, d = v
         reynolds = (settings.water.density * vel * d) / settings.water.dynamic_viscosity
         f = (-1.8 * np.log10((settings.piping.roughness / (3.7 * d)) ** 1.11 + 6.9 / reynolds))**-2
@@ -70,25 +75,31 @@ def sts(model: pyo.ConcreteModel,
         eq2 = mass_lin - settings.water.density * vel * (np.pi / 4) * d ** 2
         return [eq1, eq2]
 
-    m_lin = (p_lin_opt*1000)/(settings.water.heat_capacity_cp * (supply_temp_opt - return_temp_opt))
+    m_lin = (p_lin_opt*1000
+             / (settings.water.heat_capacity_cp * (supply_temp_opt - return_temp_opt)))
 
+    # Calculate the diameter and velocity for each pipe
     for h in range(a_i_shape_opt[1]):
-        mass_lin = m_lin[h]
-        v_lin2[h], d_lin2[h] = fsolve(equations, (0.5, 0.02))
+        mass_lin = m_lin[h]  # mass flow
+        sol = root(equations, (0.5, 0.02),
+                   method='lm')
+        if sol.success:
+            v_lin2[h], d_lin2[h] = sol.x
+        else:
+            print(h, 'Failed to calculate diameter and velocity')
 
-    res = dict(
-        a_i=a_i_opt,
-        a_p=a_p_opt,
-        a_c=a_c_opt,
-        q_c=matrices['q_c'],
-        l_i=l_i_opt,
-        d_i_0=d_lin2,
-        m_i_0=m_lin,
-        position=pos_opt,
-        # reshape p_lin_opt to a 2D array with 1 column
-        p=p_lin_opt
-    )
-
+    # Create a dictionary with the results
+    res = {
+        'a_i': a_i_opt,
+        'a_p': a_p_opt,
+        'a_c': a_c_opt,
+        'q_c': matrices['q_c'],
+        'l_i': l_i_opt,
+        'd_i_0': d_lin2,
+        'm_i_0': m_lin,
+        'position': pos_opt,
+        'p': p_lin_opt
+    }
     return res
 
 
@@ -128,7 +139,8 @@ def to_networkx_graph(matrices):
     # edge_labels = dict()
     # Add the edges to the graph
     for k in range(matrices['a_i'].shape[1]):
-        s = np.where(matrices['a_i'][:, k] == 1)[0][0], np.where(matrices['a_i'][:, k] == -1)[0][0]        
+        s = (np.where(matrices['a_i'][:, k] == 1)[0][0],
+             np.where(matrices['a_i'][:, k] == -1)[0][0])   
         G.add_edge(s[0], s[1],
                    weight=matrices['l_i'][k],
                    d=matrices['d_i_0'][k],
