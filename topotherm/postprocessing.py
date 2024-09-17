4e46583c0911403081bc748595bba5725d70dbf3
jylambert <153106800+jylambert@users.noreply.github.com>
2024-09-17 12:39:37 +0200

    Finalised mts preprocessing, added comments and todo's

--- topotherm/postprocessing.py
+++ topotherm/postprocessing.py
@@ -101,8 +101,8 @@ def sts(model: pyo.ConcreteModel,
         # values lambda_ij for ji. This is necessary for the postprocessing.
         elif lambda_ji[q] == 1:
             matrices['a_i'][:, q] = matrices['a_i'][:, q] * (-1)
-            lambda_ij[q] = 1
-            lambda_ji[q] = 0
+            lambda_ij[q] = 1    # @TODO: For sts this can be removed
+            lambda_ji[q] = 0    # @TODO: For sts this can be removed
 
     p_lin = p_ij + p_ji  # Power of the pipes
 
@@ -147,8 +147,8 @@ def sts(model: pyo.ConcreteModel,
         a_c=a_c_opt,
         q_c=q_c_opt,
         l_i=l_i_opt,
-        lambda_ij_opt=lambda_ij,
-        lambda_ji_opt=lambda_ji,
+        lambda_ij_opt=lambda_ij,  #@TODO: Remove lambda_ij_opt and lambda_ji_opt? -> lambda_ij_opt should only
+        lambda_ji_opt=lambda_ji,  #@TODO: contain 1 and lamdba_ji_opt only 0 (for sts!).
         d_i_0=d_lin,
         m_i_0=m_lin,
         position=pos_opt,
@@ -161,7 +161,7 @@ def sts(model: pyo.ConcreteModel,
 def mts(model: pyo.ConcreteModel,
         matrices: dict,
         settings: Settings):
-    """Postprocessing for the mutiple time step model. This includes the
+    """Postprocessing for the multiple time step model. This includes the
     calculation of the diameter and velocity of the pipes, the elimination of
     unused pipes and nodes.
 
@@ -330,98 +330,3 @@ def to_networkx_graph(matrices):
     # drop all edges with p=0
     G.remove_edges_from([(u, v) for u, v, d in G.edges(data=True) if d['p'] == 0])
     return G
-
-
-def mts(model, matrices, sets, t_supply, t_return):
-    """returns all matrices and results for further processing. Essentially,
-    it simplifies the results from the optimization, including pipe diameter
-    and mass flow, eliminating the unused pipes and nodes.
-
-    Args:
-        model (pyomo.environ.ConcreteModel): solved pyomo model
-        matrices (dict): dict containing the incidence matrices
-        sets (dict): dict containing the sets
-        temperatures (dict): dict containing the supply and return temperatures
-    
-    Returns:
-        dict: dict containing the updated matrices, including diameter and mass flow
-    """
-    data_dict = {}
-
-    p_cap = np.zeros([sets['a_i_shape'][1]])
-    lambda_built = np.zeros([sets['a_i_shape'][1]])
-    lambda_dir = np.zeros([sets['a_i_shape'][1], len(model.set_t)])
-    p_source_built = np.zeros(sets['a_p_shape'][1])
-
-    for v in model.component_objects(pyo.Var, active=True):
-        var_dict = {(v.name, index): pyo.value(v[index]) for index in v}
-        data_dict.update(var_dict)
-        if v.name == "lambda_built":
-            for index in v:
-                lambda_built[index] = pyo.value(v[index])
-        if v.name == "lambda_dir_1":
-            for index in v:
-                lambda_dir[index] = pyo.value(v[index])
-        if v.name == "P_cap":
-            for index in v:
-                p_cap[index] = pyo.value(v[index])
-        if v.name == "P_source_cap":
-            for index in v:
-                p_source_built[index] = pyo.value(v[index])
-
-    lambda_built = np.around(lambda_built, 0)
-    lambda_dir = np.around(lambda_dir, 0)
-
-    # @TODO for all timesteps (with new formulation)
-    for q, _ in enumerate(lambda_dir_1):
-        if lambda_built[q] == 0:
-            matrices['a_i'][:, q] = 0
-            matrices['l_i'][q] = 0
-        elif (lambda_built[q] == 1) & (lambda_dir_1[q, 0] == 0):
-            matrices['a_i'][:, q] = matrices['a_i'][:, q] * (-1)
-            lambda_dir_1[q, np.where(lambda_dir_1[q, 1:] == 0)[0]] = 1
-            lambda_dir_2[q, np.where(lambda_dir_2[q, 1:] == 1)[0]] = 0
-
-    p_cap_opt = np.delete(p_cap, np.where(~matrices['a_i'].any(axis=0)))
-    pos_opt = np.delete(matrices['position'], np.where(~matrices['a_i'].any(axis=1)), axis=0)
-    a_c_opt = np.delete(matrices['a_c'], np.where(~matrices['a_i'].any(axis=1)), axis=0)
-    a_p_opt = np.delete(matrices['a_p'], np.where(~matrices['a_i'].any(axis=1)), axis=0)
-    a_i_opt = matrices['a_i']
-    a_i_opt = np.delete(a_i_opt, np.where(~a_i_opt.any(axis=0)), axis=1)
-    a_i_opt = np.delete(a_i_opt, np.where(~a_i_opt.any(axis=1)), axis=0)
-    l_i_opt = matrices['l_i'][matrices['l_i'] != 0]
-
-    a_i_shape_opt = np.shape(a_i_opt)  # (rows 0, columns 1)
-    d_lin2 = np.zeros(a_i_shape_opt[1])
-    v_lin2 = np.zeros(a_i_shape_opt[1])
-    supply_temp_opt = np.ones(a_i_shape_opt[1]) * t_supply
-    return_temp_opt = np.ones(a_i_shape_opt[1]) * t_return
-
-    def equations(v):
-        vel, d = v
-        reynolds = (settings.Water.density * vel * d) / settings.Water.dynamic_viscosity
-        f = (-1.8 * np.log10((settings.Piping.roughness / (3.7 * d)) ** 1.11 + 6.9 / reynolds))**-2
-        eq1 = vel - np.sqrt((2 * settings.Piping.max_pr_loss * d) / (f * settings.Water.density))
-        eq2 = mass_lin - settings.Water.density * vel * (np.pi / 4) * d ** 2
-        return [eq1, eq2]
-
-    m_lin = (p_cap_opt*1000)/(settings.Water.heat_capacity_cp * (supply_temp_opt - return_temp_opt))
-
-    for h in range(a_i_shape_opt[1]):
-        mass_lin = m_lin[h]
-        v_lin2[h], d_lin2[h] = fsolve(equations, (0.5, 0.02))
-
-    res = dict(
-        a_i=a_i_opt,
-        a_p=a_p_opt,
-        a_c=a_c_opt,
-        q_c=matrices['q_c'],
-        l_i=l_i_opt,
-        d_i_0=d_lin2,
-        m_i_0=m_lin,
-        position=pos_opt,
-
-    )
-
-    return res
-
