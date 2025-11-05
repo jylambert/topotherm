6b6c849d1faa9c04e4c7e3045c43d8b17d92d642
Amedeo Ceruti <amedeo.ceruti@tum.de>
2025-11-05 13:33:51 +0100

    breaking changes to restructure repo, naming
    - Simultaneity is not supported anymore (for now)
    - all models moved to topotherm/models
    - renamed multiple_timesteps to multi_timesteps
    - modified init files to import the new filenames and folder

--- topotherm/__init__.py
+++ topotherm/__init__.py
@@ -3,9 +3,6 @@ from . import fileio
 from . import precalculation_hydraulic
 from . import postprocessing
 from . import settings
-from . import single_timestep
-from . import multiple_timestep
-from . import sets
+from . import models
 from . import plotting
 from . import create_matrices
-from . import simultaneity
---
+++ topotherm/models/__init__.py
@@ -0,0 +1,3 @@
+from . import multi_timestep
+from . import single_timestep
+from . import sets
