d63df41b8659fd468aaccc9a6cba7e1cbc077280
Amedeo Ceruti <amedeo.ceruti@tum.de>
2025-11-26 16:06:38 +0100

    revamped sts testing
    now can be run with open source solvers in very little time due to district size. tested with several OS solvers, decided for a pip-installable one for ease.

--- tests/sts.py
+++ tests/sts.py
@@ -1,107 +1,74 @@
-import os
+from pathlib import Path
 
-import pandas as pd
 import pyomo.environ as pyo
 from pytest import approx
+import numpy as np
 
 import topotherm as tt
 
 
-def read_regression(path, i):
-    """Read the regression coefficients for the thermal capacity and heat
-    losses from csv file.
-    """
-    # read df and force floats
-    df = pd.read_csv(path, sep=",", index_col=0, header=0, dtype=float)
-    r_thermal_cap = {
-        "power_flow_max_kW": df.loc[i, "power_flow_max_kW"],
-        "a": df.loc[i, "capacity_a"],
-        "b": df.loc[i, "capacity_b"],
-        "power_flow_max_partload": df.loc[i, "power_flow_max_partload"],
-    }
-    r_heat_loss = {"a": df.loc[i, "heat_loss_a"], "b": df.loc[i, "heat_loss_b"]}
-    return r_thermal_cap, r_heat_loss
+SOLVERS = ["scip", "gurobi", "cbc", "highs", "cplex", "glpk"]
+R_COSTS = {"power_flow_max_kW": np.array([6.9e+04]),
+                 "a": 0.018377, "b": 567.335, "power_flow_max_partload": 1}
+R_LOSS = {"a": 4.348e-07, "b": 0.02189}
 
 
 def test_sts_forced(request):
     """Main function to run the optimization"""
     solver_name = request.config.getoption("--solver")
-    assert solver_name in [
-        "scip",
-        "gurobi",
-        "cbc",
-    ], f"Unsupported solver: {solver_name}"
+    assert solver_name in SOLVERS, f"Unsupported solver: {solver_name}"
     # Load the district
-    current_path = os.path.dirname(os.path.abspath(__file__))
-    mat = tt.fileio.load(os.path.join(current_path, "data_sts"))
-
-    # regression
-    r_thermal_cap, r_heat_loss = read_regression(
-        os.path.join(current_path, "data_sts", "regression.csv"), 0
-    )
+    datapath = Path(__file__).resolve().parent / "test_data"
+    mat = tt.fileio.load(datapath)
 
     # import settings
-    settings = tt.settings.load(os.path.join(current_path, "data_sts", "config.yaml"))
+    settings = tt.settings.load(datapath / "config.yaml")
 
-    model_sets = tt.sets.create(mat)
-    model = tt.single_timestep.model(
+    model_sets = tt.models.sets.create(mat)
+    model = tt.models.single_timestep.create(
         matrices=mat,
         sets=model_sets,
-        regression_inst=r_thermal_cap,
-        regression_losses=r_heat_loss,
+        regression_inst=R_COSTS,
+        regression_losses=R_LOSS,
         economics=settings.economics,
         optimization_mode="forced",
     )
 
     # Optimization initialization
     opt = pyo.SolverFactory(solver_name)
-    opt.options["mipgap"] = 0.01
-    opt.options["timelimit"] = 600
+    opt.options["mipgap"] = 0.0001
 
     result = opt.solve(model, tee=True)
     assert result.solver.status == pyo.SolverStatus.ok
-    # assert that the objective value is less than 2% away from the expected
-    # value
-    assert pyo.value(model.obj) == approx(519676.4358105995, rel=0.02)
+
+    tt.postprocessing.sts(model, mat, settings)
+    assert pyo.value(model.obj) == approx(-990.8, rel=0.01)
 
 
 def test_sts_eco(request):
     """Main function to run the optimization"""
     solver_name = request.config.getoption("--solver")
-    assert solver_name in [
-        "scip",
-        "gurobi",
-        "cbc",
-    ], f"Unsupported solver: {solver_name}"
+    assert solver_name in SOLVERS, f"Unsupported solver: {solver_name}"
 
-    # Load the district
-    current_path = os.path.dirname(os.path.abspath(__file__))
-    mat = tt.fileio.load(os.path.join(current_path, "data_sts"))
+    datapath = Path(__file__).resolve().parent / "test_data"
+    mat = tt.fileio.load(datapath)
+    settings = tt.settings.load(datapath / "config.yaml")
 
-    # regression
-    r_thermal_cap, r_heat_loss = read_regression(
-        os.path.join(current_path, "data_sts", "regression.csv"), 0
-    )
-
-    # import settings
-    settings = tt.settings.load(os.path.join(current_path, "data_sts", "config.yaml"))
-
-    model_sets = tt.sets.create(mat)
-    model = tt.single_timestep.model(
+    model_sets = tt.models.sets.create(mat)
+    model = tt.models.single_timestep.create(
         matrices=mat,
-        sets=model_sets,
-        regression_inst=r_thermal_cap,
-        regression_losses=r_heat_loss,
+        sets=model_sets,    
+        regression_inst=R_COSTS,
+        regression_losses=R_LOSS,
         economics=settings.economics,
         optimization_mode="economic",
     )
 
     # Optimization initialization
     opt = pyo.SolverFactory(solver_name)
-    opt.options["mipgap"] = 0.01
-    opt.options["timelimit"] = 600
+    opt.options["mipgap"] = 0.0001
 
     result = opt.solve(model, tee=True)
     assert result.solver.status == pyo.SolverStatus.ok
-    assert pyo.value(model.obj) == approx(-4.01854e04, rel=0.02)
-    # assert (abs(pyo.value(model.obj)) - 4.01854e+04) < 0.02 * 4.01854e+04
+    assert pyo.value(model.obj) == approx(-1436, rel=0.01)
+    tt.postprocessing.sts(model, mat, settings)
